6a95f8b4a7045bd12856ea5496b419d2ab4f6e5a
jiangzhe <jiangzhe@sanjiang365.com>
2016-09-22 16:34:42 +0800

    添加从springcloud读取配置文件

--- src/main/java/olymtech/api/user/UserApiApplication.java
+++ src/main/java/olymtech/api/user/UserApiApplication.java
@@ -2,16 +2,13 @@ package olymtech.api.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 /**
  * Created by JiangZhe on 16/3/30.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EntityScan(basePackageClasses = {UserApiApplication.class, Jsr310JpaConverters.class})
 public class UserApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
